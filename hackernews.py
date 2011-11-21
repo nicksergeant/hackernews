38a168e8b2824124a7a10c2be8ba68c8c97d070e
Nick Sergeant <nick@nicksergeant.com>
2011-11-20 20:04:42 -0500

    Support grabbing all saved items

--- hackernews.py
+++ hackernews.py
@@ -22,11 +22,15 @@ def _login(args):
 
     return r.cookies
 
-def _get_saved_stories(args):
+def _get_saved_stories(args, r=None, saved=None):
     """Returns a sorted list of the user's saved stories."""
 
-    cookies = _login(args)
-    r = requests.get('http://news.ycombinator.com/saved?id=%s' % args.username, cookies=cookies)
+    if r is None:
+        cookies = _login(args)
+        r = requests.get('http://news.ycombinator.com/saved?id=%s' % args.username, cookies=cookies)
+        saved = []
+    else:
+        cookies = r.cookies
 
     J = pq(r.content)
     stories = J('table table td.title')
@@ -34,14 +38,30 @@ def _get_saved_stories(args):
     for story in stories:
         title = J(story).text()
         if not re.match('\d+\.|More', title):
-            print title
 
-    return []
+            url = J('a', story).attr('href')
+
+            if not url.startswith('http'):
+                url = 'https://news.ycombinator.com/' + url
+
+            saved.append({
+                'title': title,
+                'url': url,
+            })
+
+    if args.all:
+        last = J('a', J('table table tr td.title:last'))
+        if last.text() == 'More':
+            r = requests.get('https://news.ycombinator.com%s' % last.attr('href'), cookies=cookies)
+            _get_saved_stories(args, r=r, saved=saved)
+
+    return saved
 
 def saved(args):
     """Returns a formatted list of the logged-in user's saved stories."""
 
     stories = _get_saved_stories(args)
+    print stories
 
     return ''
 
@@ -54,6 +74,7 @@ subparsers = parser.add_subparsers()
 saved_parser = subparsers.add_parser('saved')
 saved_parser.add_argument('-u', '--username', dest='username', help='HN Username', required=True)
 saved_parser.add_argument('-p', '--password', dest='password', help='HN Password', required=True)
+saved_parser.add_argument('--all', dest='all', help='Get all saved stories?', action='store_true')
 saved_parser.set_defaults(func=saved)
 
 # Args
