139c879d08461faf6399806afa510d6c4a070bca
Nick Sergeant <nick@nicksergeant.com>
2011-11-20 19:39:45 -0500

    Working saved-items feed

--- hackernews.py
+++ hackernews.py
@@ -1,23 +1,48 @@
 #!/usr/bin/env python
 
-import argparse
+import argparse, re, requests
+from pyquery import PyQuery as pq
 
 
 def _login(args):
     """Log in to Hacker News and return the cookies."""
 
-    r = requests.get('http://news.ycombinator.com/newslogin')
+    r = requests.get('https://news.ycombinator.com/newslogin')
+    J = pq(r.content)
 
-    return ''
+    fnid = J('input[name="fnid"]').val()
+
+    payload = {
+        'fnid': fnid,
+        'u': args.username,
+        'p': args.password,
+    }
+
+    r = requests.post('https://news.ycombinator.com/y', data=payload)
+
+    return r.cookies
 
 def _get_saved_stories(args):
     """Returns a sorted list of the user's saved stories."""
-    user = _login(args)
+
+    cookies = _login(args)
+    r = requests.get('http://news.ycombinator.com/saved?id=%s' % args.username, cookies=cookies)
+
+    J = pq(r.content)
+    stories = J('table table td.title')
+
+    for story in stories:
+        title = J(story).text()
+        if not re.match('\d+\.|More', title):
+            print title
+
     return []
 
 def saved(args):
     """Returns a formatted list of the logged-in user's saved stories."""
+
     stories = _get_saved_stories(args)
+
     return ''
 
 # Parser
@@ -27,8 +52,8 @@ subparsers = parser.add_subparsers()
 
 # Subparsers
 saved_parser = subparsers.add_parser('saved')
-saved_parser.add_argument('--username', help='HN Username', required=True)
-saved_parser.add_argument('--password', help='HN Password', required=True)
+saved_parser.add_argument('-u', '--username', dest='username', help='HN Username', required=True)
+saved_parser.add_argument('-p', '--password', dest='password', help='HN Password', required=True)
 saved_parser.set_defaults(func=saved)
 
 # Args
